d466fb50d4fcc9425837b887d8dc00c1118d75fe
Kavya Prabahar <144550263+kavya-prabahar@users.noreply.github.com>
2024-08-12 11:44:21 +0530

    Integrated JS for exercise 1

---
+++ Exercise 1/scripts.js
@@ -0,0 +1,32 @@
+
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+function addToCart(productName, productPrice) {
+    cart.push({ name: productName, price: productPrice });
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    alert(`${productName} added to cart!`);
+}
+
+function displayCartItems() {
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    const productDiv = document.querySelector('.product p');
+    const costDiv = document.querySelector('.cost p');
+    let totalCost = 0;
+
+    cartItems.forEach(item => {
+        productDiv.innerHTML += `${item.name}<br>`;
+        costDiv.innerHTML += `$${item.price}<br>`;
+        totalCost += item.price;
+    });
+
+    productDiv.innerHTML += `<b>Total</b><br>`;
+    costDiv.innerHTML += `<b>$${totalCost}</b><br>`;
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', displayCartItems);
+} else {
+    displayCartItems();
+}
